2364ce75ae3aa03fd0ff1ae86aed6f05ba6d8bf3
amandasoaresv <amandasoaresvieira5@gmail.com>
2025-10-06 09:51:40 -0300

    feat: conectando rota 'POST/links' ao banco de dados com drizzle

--- source/api/src/modules/links/link.repository.js
+++ source/api/src/modules/links/link.repository.js
@@ -1,17 +1,36 @@
+import db from '../../infra/database.js';
+import { randomUUID } from 'node:crypto';
+import { links } from '../../infra/db/schema.js';
+
 export class LinkRepository {
   links = [];
-  constructor() {}
+  constructor() {
+    this.db = db;
+  }
 
   async findAll() {
     return this.links;
   }
 
   async create(linksData) {
-    this.links.push(linksData);
-    return linksData;
+    let result;
+    try {
+      const id = randomUUID();
+      result = await this.db
+        .insert(links)
+        .values({
+          id,
+          ...linksData,
+        })
+        .returning();
+      console.log
+    } catch (error) {
+      console.error('Erro ao cliar link:', error);
+    }
+    return result[0];
   }
 
-  async update(id, clinksData) {}
+  async update(id, clinksData) { }
 
-  async remove(id) {}
+  async remove(id) { }
 }

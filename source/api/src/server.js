7e4bf0ed0af2392b4dd056584ffb256a55ba1ab7
Matheus <matheusteodorogarcia@gmail.com>
2025-10-08 10:57:24 -0300

    refactor: coloca variável ambiente 'PORT' na env

--- source/api/src/server.js
+++ source/api/src/server.js
@@ -1,16 +1,15 @@
+import "dotenv/config";
 import fastify from "fastify";
 import { linksRoutes } from "./modules/links/link.route.js";
 
-const port = 3000;
-
 const server = fastify({ logger: true });
 
-server.register(linksRoutes, { prefix: "/api" });
+server.register(linksRoutes, { prefix: "/" });
 
-server.listen({ port }, (error) => {
+server.listen({ port: process.env.PORT }, (error) => {
   if (error) {
     console.error("Erro ao executar servidor:", error);
     process.exit(1);
   }
-  console.log(`Servidor executando na porta ${port}`);
+  console.log(`Servidor executando na porta ${process.env.PORT}`);
 });

053bfdb0b783f757a7f700b9d744f3463e7e7e7d
Matheus <matheusteodorogarcia@gmail.com>
2025-10-10 11:35:53 -0300

    feat: adicona cors no backend

--- source/api/src/server.js
+++ source/api/src/server.js
@@ -1,8 +1,14 @@
 import "dotenv/config";
 import fastify from "fastify";
 import { linksRoutes } from "./modules/links/link.route.js";
+import cors from "@fastify/cors";
 
 const server = fastify({ logger: true });
+
+await server.register(cors, {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+});
 
 server.register(linksRoutes, { prefix: "/" });
 

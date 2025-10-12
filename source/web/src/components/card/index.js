05d9a26e9a0962181f0a26918249ea3d199b853d
amandasoaresv <amandasoaresvieira5@gmail.com>
2025-10-11 22:06:52 -0300

    feat: conecta front-end com a rota 'PUT /links/:id' do back-end

--- source/web/src/components/card/index.js
+++ source/web/src/components/card/index.js
@@ -1,6 +1,7 @@
 "use client";
 import axios from "axios";
 import styles from "./card.module.css";
+import React from "react";
 import {
   Clipboard,
   Calendar,
@@ -10,6 +11,12 @@ import {
 } from "lucide-react";
 
 export default function Card({ data, onDelete }) {
+  const [editActive, setEditActive] = React.useState(false);
+  const [editData, seteditData] = React.useState({
+    legenda: data.legenda,
+    urlOriginal: data.urlOriginal,
+  });
+
   async function handleDelete() {
     try {
       await axios.delete(`http://localhost:3333/links/${data.id}`);
@@ -19,25 +26,71 @@ export default function Card({ data, onDelete }) {
     }
   }
 
+  async function handleEdit(event) {
+    event.preventDefault();
+    try {
+      await axios.put(`http://localhost:3333/links/${data.id}`, editData);
+      window.location.reload();
+    } catch (error) {
+      console.error("Erro ao editar link:", error);
+    }
+  }
+
   return (
     <div className={styles.card}>
-      <div className={styles.topo}>
-        <h2>{data.legenda}</h2>
-        <span>
-          <ChartColumnBig size={20} /> {data.cliques}
-        </span>
-      </div>
+      {!editActive && (
+        <>
+          <div className={styles.topo}>
+            <h2>{data.legenda}</h2>
+            <span>
+              <ChartColumnBig size={20} /> {data.cliques}
+            </span>
+          </div>
 
-      <a
-        href={`http://localhost:3333/${data.codigo}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles.urlCurta}
-      >
-        http://localhost:3333/{data.codigo}
-      </a>
+          <a
+            href={`http://localhost:3333/${data.codigo}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.urlCurta}
+          >
+            http://localhost:3333/{data.codigo}
+          </a>
 
-      <p className={styles.urlOriginal}>{data.urlOriginal}</p>
+          <p className={styles.urlOriginal}>{data.urlOriginal}</p>
+        </>
+      )}
+
+      {editActive && (
+        <>
+          <form className={styles.form} onSubmit={(event) => handleEdit(event)}>
+            <input
+              id="legenda"
+              type="text"
+              placeholder="Legenda"
+              value={editData.legenda || ""}
+              className={styles.input}
+              onChange={({ target }) =>
+                seteditData({ ...editData, legenda: target.value })
+              }
+            />
+
+            <input
+              id="url"
+              type="text"
+              placeholder="Link"
+              value={editData.urlOriginal || ""}
+              className={styles.input}
+              onChange={({ target }) =>
+                seteditData({ ...editData, urlOriginal: target.value })
+              }
+            />
+
+            <button type="submit" className={styles.button}>
+              Editar
+            </button>
+          </form>
+        </>
+      )}
 
       <div className={styles.rodape}>
         <p className={styles.data}>
@@ -47,7 +100,7 @@ export default function Card({ data, onDelete }) {
           <button className={styles.copiar}>
             <Clipboard size={18} /> Copiar
           </button>
-          <button>
+          <button onClick={() => setEditActive(!editActive)}>
             <Pencil size={18} className={styles.pencil} />
           </button>
           <button onClick={handleDelete}>

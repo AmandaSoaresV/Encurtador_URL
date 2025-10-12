8e8ca45514774853c266a8253d2ee5457d6a5b1c
amandasoaresv <amandasoaresvieira5@gmail.com>
2025-10-11 21:05:25 -0300

    feat: conecta front-end com a rota 'POST /links' do back-end

--- source/web/src/components/form/index.js
+++ source/web/src/components/form/index.js
@@ -1,8 +1,24 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./form.module.css";
+import axios from "axios";
 
 export default function Form() {
+  const [data, setData] = useState({ legenda: "", urlOriginal: "" });
+
+  async function handleCreate(event) {
+    event.preventDefault();
+    try {
+      await axios.post(`http://localhost:3333/links`, data);
+      window.location.reload();
+    } catch (error) {
+      console.error("Erro ao deletar link:", error);
+    }
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={(event) => handleCreate(event)}>
       <label htmlFor="legenda" className={styles.titulo3}>
         Legenda do link *
       </label>
@@ -11,6 +27,7 @@ export default function Form() {
         type="text"
         placeholder="Ex: meu portifólio, site da empresa.."
         className={styles.input}
+        onChange={({ target }) => setData({ ...data, legenda: target.value })}
       />
 
       <label htmlFor="url" className={styles.titulo3}>
@@ -21,6 +38,9 @@ export default function Form() {
         type="text"
         placeholder="https://exemplo.com/URLlonga"
         className={styles.input}
+        onChange={({ target }) =>
+          setData({ ...data, urlOriginal: target.value })
+        }
       />
 
       <button type="submit" className={styles.button}>

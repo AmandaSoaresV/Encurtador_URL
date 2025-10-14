d69977617ccd4151216d30d874b3643dc3bb57ce
amandasoaresv <amandasoaresvieira5@gmail.com>
2025-10-14 14:55:58 -0300

    chore: prepara ambiente para deploy

--- source/api/src/server.js
+++ source/api/src/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import fastify from "fastify";
 import { linksRoutes } from "./modules/links/link.route.js";
 import cors from "@fastify/cors";
+import serverless from "@fastify/aws-lambda";
 
 const server = fastify({ logger: true });
 
@@ -12,10 +13,12 @@ await server.register(cors, {
 
 server.register(linksRoutes, { prefix: "/" });
 
-server.listen({ port: process.env.PORT }, (error) => {
-  if (error) {
-    console.error("Erro ao executar servidor:", error);
-    process.exit(1);
-  }
-  console.log(`Servidor executando na porta ${process.env.PORT}`);
-});
+export const handler = serverless(app);
+
+// server.listen({ port: process.env.PORT }, (error) => {
+//   if (error) {
+//     console.error("Erro ao executar servidor:", error);
+//     process.exit(1);
+//   }
+//   console.log(`Servidor executando na porta ${process.env.PORT}`);
+// });

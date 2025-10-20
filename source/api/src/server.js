3cc8a2e8bc1b134fa02a52761930b16fa8845401
amandasoaresv <amandasoaresvieira5@gmail.com>
2025-10-20 10:22:42 -0300

    fix: retirando serverless do server.js

--- source/api/src/server.js
+++ source/api/src/server.js
@@ -2,7 +2,6 @@ import "dotenv/config";
 import fastify from "fastify";
 import { linksRoutes } from "./modules/links/link.route.js";
 import cors from "@fastify/cors";
-import serverless from "@fastify/aws-lambda";
 
 const server = fastify({ logger: true });
 
@@ -13,12 +12,10 @@ await server.register(cors, {
 
 server.register(linksRoutes, { prefix: "/" });
 
-export const handler = serverless(app);
-
-// server.listen({ port: process.env.PORT }, (error) => {
-//   if (error) {
-//     console.error("Erro ao executar servidor:", error);
-//     process.exit(1);
-//   }
-//   console.log(`Servidor executando na porta ${process.env.PORT}`);
-// });
+server.listen({ port: process.env.PORT }, (error) => {
+  if (error) {
+    console.error("Erro ao executar servidor:", error);
+    process.exit(1);
+  }
+  console.log(`Servidor executando na porta ${process.env.PORT}`);
+});
